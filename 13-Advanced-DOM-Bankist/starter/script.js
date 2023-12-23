bfd7492dad653c438a5cea6f4e6d40c81e8717c8
Maged Saif <magedsaif21@gmail.com>
2023-12-23 16:14:10 +0200

    Lec 5. Selecting, Creating AND Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,3 +30,59 @@ document.addEventListener('keydown', function (e) {
   }
 });
 // Dom is the interface between our code and the browser (HTML Documents that is rendered in and by the browser)
+// we can use it to make  hs interact with the browser
+// DOM is a very complex API that contains methods and properties to interact with the DOM tree
+////////////////////////////////////////////////////////////////////
+//Lec 5. Selecting, Creating AND Deleting Elements
+// -----------------------------------------------------------------
+console.log(document.documentElement);
+console.log(document.body);
+console.log(document.head);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); // return HTML collection(live collection) that means that if the DOM changes so that collection would be changed automatically that doesnt work with a node list
+
+document.getElementsByClassName('btn'); // return live collection
+
+// Creating and Inserting elements
+// quick and easy way to create elements
+//.insertAdjacentHTML
+// but we could use another method to bulid the element from scratch more programatically
+
+// programatically build an element
+const message = document.createElement('div'); // live element
+message.classList.add('cookie-message');
+// message.textContent = 'we use cookies for improved functionality and analytics';
+message.innerHTML =
+  'we use cookies for improved functionality and analytics <button class="btn btn--close-cookie">Got it!</button>';
+
+// we have to insert it in our DOM -insert in our header element-
+// prepend adds the element as the first child of the header element
+// header.prepend(message);
+// we could also add as the last child element to the header element
+header.append(message); // this will be applied cause its like the person that cannot be at two places at the same time
+
+// what if we want to insert multiple copies of the same element
+// header.append(message.cloneNode(true));
+// that gave me the message in two places
+
+// header.before(message);
+// header.after(message);
+
+// delete elements
+// remove the element when we click a button
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove(); // its recent, the other way
+    // message.parentElement.removeChild(message); (DOM traversing)
+  });
+
+
+  //////////////////////////////////////////////////////////////////
