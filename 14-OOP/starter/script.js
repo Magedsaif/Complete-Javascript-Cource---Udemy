3aa411f61a87641b3d8397fca40740e5ac42d80a
Maged Saif <magedsaif21@gmail.com>
2024-01-06 12:39:43 +0200

    Coding Challenge #4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -620,25 +620,24 @@ class Account {
     this.deposit(-val);
     return this;
   }
-  
-  
+
   // in the public interface we only need this method, not the other methods
   requestLoan(val) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan Approved`);
     }
-    return this
+    return this;
   }
-  
+
   // 4) private methods
   // we could use the hash symbol to make it private method as well but its not supported yet in the browsers so we will use the underscore instead to make it private method and not to be used outside the class. but its not a real private method because we could still access it from outside the class but its just a convention to not use it outside the class.
 
   // #approveLoan(val) {
 
   _approveLoan(val) {
-  return true;
-}
+    return true;
+  }
 }
 
 // const acc1 = new Account('jonas', 'EURO', 1111);
@@ -673,7 +672,6 @@ class Account {
 // new feuture to be  added to the language Class fields, in the proposal stage now but we can use it with babel and webpack and other tools to use it now. so we will use it here.
 // in conventional oop languages we call the properties fields, but in JS we call them class fields. so we will use the hash symbol to make them private fields.
 
-
 //console.log(acc1.#movements); // uncaught SyntaxError: Private field '#movements' must be declared in an enclosing class
 
 // console.log(acc1.#pin); // uncaught SyntaxError: Private field '#pin' must be declared in an enclosing class
@@ -687,3 +685,94 @@ class Account {
 // this would not work unless we return this(which is the object itself using the method) from each and every method used down here
 // acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 // console.log(acc1.getMovements());
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCL extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 0.01 * this.#charge;
+    console.log(
+      `Tesla going at ${this.speed} km/h, with a charge of ${this.#charge}`
+    );
+    return this;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+}
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// // Linking the prototypes
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge -= 0.01 * this.charge;
+//   console.log(`Tesla going at ${this.speed} km/h, with a charge of ${this.charge}`);
+// };
+
+// const Tesla = new EV('Tesla', 120, 23);
+
+const rivian = new EVCL('Rivian', 120, 30);
+// console.log(rivian.#charge);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+  console.log(rivian.speedUS);
