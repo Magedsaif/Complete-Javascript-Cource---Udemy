f0260ef477b93b914e9d7a3ef215fe76f1a756c8
Maged Saif <magedsaif21@gmail.com>
2024-01-10 22:39:38 +0200

    first AJAX call (XMLHttpRequest)

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -19,15 +19,46 @@ const countriesContainer = document.querySelector('.countries');
 
 // "online" API: Application running on a server, that receives requests for data, and sends data back as response.
 
-
 // we can build our own API (requires backend development skills) or use a third party API.
 //  XMLHttpRequest (XHR) object: API in the form of an object, provided to us by the browser, to be used by client-side scripts like JavaScript. It is used to make HTTP requests to the server. It can send data to, and receive data from, a web server.
 // we dont use XML anymore, we use JSON (JavaScript Object Notation) instead.
+/////////////////////////////////////////////////////////////////////
+// our first AJAX call (XMLHttpRequest)
+//------------------------------------------------------------------
+
+// Old school way
+const getCountryData =function(country){
+const request = new XMLHttpRequest();
+
+request.open('GET', `https://restcountries.com/v2/name/${country}`);
+request.send(); //here we send the request which will fetch the data in the background once its done it will emmit the load event, so as soon the data arrives the call back function in the add event listner will be called
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `<article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+})}
 
 
-
-
-
-
-
+getCountryData('egypt');
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
 
