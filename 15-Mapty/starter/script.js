0ab35958d8486d65392c54ac16807d978b5b9854
Maged Saif <magedsaif21@gmail.com>
2024-01-10 19:50:21 +0200

    edit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -42,68 +42,160 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
-if (navigator.geolocation)
-  // Geolocation API
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // using destructuring
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in minutes
+  }
+}
 
-      const coords = [latitude, longitude];
-      map = L.map('map').setView(coords, 13);
-      console.log(map);
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
 
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
 
-      // handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus(); // start on distance
-      });
-    },
-    function () {
-      alert('Could not get your position');
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+// console.log(run1, cycling1);
+
+//////////////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
+class App {
+  // private instance proprties
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      // Geolocation API
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+  }
+
+  _loadMap(position) {
+    // using destructuring
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+    this.#map = L.map('map').setView(coords, 13);
+    // console.log(map);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus(); // start on distance
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    e.preventDefault();
+
+    // Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    // if activity running, create a running object
+    if (type === 'running') {
+      // check if data is valid
+      const cadence = +inputCadence.value;
+      if (
+        !Number.isFinite(distance) ||
+        !Number.isFinite(distance) ||
+        !Number.isFinite(cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
     }
-  );
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  //clear input feilds after submitting
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-  // Display Marker
-  console.log(mapEvent);
-  // using destructuring
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('workout')
-    .openPopup();
-});
+    // if activity cycling, create a cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+    }
+    // Add new object to workout array
 
-// toggle between cadence and elevation gain based on workout type selected (running or cycling) using event delegation on the parent element of the two input fields (form__row) and listening for the click event on the parent element and then checking if the target element is the inputType element and then toggling the hidden class on the two input fields (inputCadence and inputElevation) using the closest method to select the parent element of the two input fields (form__row) and then using the toggle method to toggle the hidden class on the parent element of the two input fields (form__row) which will toggle the hidden class on the two input fields (inputCadence and inputElevation) as well. The hidden class is used to hide the two input fields (inputCadence and inputElevation) by setting the display property to none.
+    // Render workout on map as marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('workout')
+      .openPopup();
+    // Render workout on list
 
-inputType.addEventListener('click', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+    // Hide form + clear input fields
+
+    //clear input feilds after submitting
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    // Display Marker
+    // using destructuring
+  }
+}
+
+const app = new App();
