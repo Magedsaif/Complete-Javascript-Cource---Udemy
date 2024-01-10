7fd156cf9f7f7302f266f725289090e2f83c2be4
Maged Saif <magedsaif21@gmail.com>
2024-01-10 23:23:03 +0200

    Refactor code to remove old school way of making API requests and implement callback hell solution

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -27,7 +27,7 @@ const countriesContainer = document.querySelector('.countries');
 //------------------------------------------------------------------
 
 // Old school way
-const getCountryData =function(country){
+/* const getCountryData =function(country){
 const request = new XMLHttpRequest();
 
 request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -57,8 +57,77 @@ request.addEventListener('load', function () {
 })}
 
 
-getCountryData('egypt');
 getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+
+*/
+
+/////////////////////////////////////////////////////////////////////
+// CALLBACK HELL
+//------------------------------------------------------------------
+
+
+
+
+
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+const getCountryAndNeighbour = function (country) {
+
+    // Ajax call country 1
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send(); //here we send the request which will fetch the data in the background once its done it will emmit the load event, so as soon the data arrives the call back function in the add event listner will be called
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // render country 1
+    renderCountry(data);
+
+    // GET neighbour country (2)
+
+    // using optional chaining to handle the case where country has no borders
+    const neighbour = data.borders?.[0];
+
+    // Ajax call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function(){
+        const data2 = JSON.parse(this.responseText);
+        console.log(data2);
+        renderCountry(data2, 'neighbour');
+    })
+  });
+};
+getCountryAndNeighbour('usa');
+
+// what if we want to get the neighbour of the neighbour country? we will have to nest another call back inside the second callback and so on. this is called callback hell.
+// callback hell: when we have a lot of nested callbacks, which makes the code hard to read and maintain.
+// we will solve this problem using promises in the next lecture.
+
+
+setTimeout(() => {
+  console.log('1 second passed');
+}, 1000); // this is an asynchronous function. it will be executed after 1 second. it will not block the code execution.
 
