7b9a01572af66d16ad173670053c8826514d4a74
Maged Saif <magedsaif21@gmail.com>
2024-01-10 21:43:33 +0200

    ASYNCHRONOUS JAVASCRIPT. AJAX AND APIS

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,31 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+//////////////////////////////////////////////////
+// ASYNCHRONOUS JAVASCRIPT. AJAX AND APIS
+// Asynchronous coordinats behavior of a program over a period of time.
+// not happining in the same time
+// we defer an action into the future making the code asynchronous
+
+// AJAX: Asynchronous JavaScript And XML: allows us to communicate with remote web servers in an asynchronous way. Allows us to request data from web servers dynamically.
+
+// API: Application Programming Interface: piece of software that can be used by another piece of software, in order to allow applications to talk to each other. In web development, an API is a piece of software that will allow two applications to talk to each other over the internet.
+
+// API is not a database or a server. It is the code with access points to the server.
+
+// some APIs are public, some are paid, some are private.
+
+// "online" API: Application running on a server, that receives requests for data, and sends data back as response.
+
+
+// we can build our own API (requires backend development skills) or use a third party API.
+//  XMLHttpRequest (XHR) object: API in the form of an object, provided to us by the browser, to be used by client-side scripts like JavaScript. It is used to make HTTP requests to the server. It can send data to, and receive data from, a web server.
+// we dont use XML anymore, we use JSON (JavaScript Object Notation) instead.
+
+
+
+
+
+
+
+
