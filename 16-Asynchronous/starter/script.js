4fd867fb6f92b1031656696501881ecc13576b9b
Maged Saif <magedsaif21@gmail.com>
2024-01-11 13:16:13 +0200

    throw errors manually

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,12 +3,12 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 const renderError = function (msg) {
-    countriesContainer.insertAdjacentText('beforeend', msg);
-    countriesContainer.style.opacity = 1;
-    }
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
 
-    const renderCountry = function (data, className = '') {
-        const html = `<article class="country ${className}">
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
                 <img class="country__img" src="${data.flag}" />
                 <div class="country__data">
                   <h3 class="country__name">${data.name}</h3>
@@ -16,16 +16,18 @@ const renderError = function (msg) {
                   <p class="country__row"><span>👫</span>${(
                     +data.population / 1000000
                   ).toFixed(1)} people</p>
-                  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                  <p class="country__row"><span>🗣️</span>${
+                    data.languages[0].name
+                  }</p>
                   <p class="country__row"><span>💰</span>${
                     data.currencies[0].name
                   }</p>
                 </div>
               </article>`;
-      
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
-      };
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 //////////////////////////////////////////////////
 // ASYNCHRONOUS JAVASCRIPT. AJAX AND APIS
 // Asynchronous coordinats behavior of a program over a period of time.
@@ -183,28 +185,72 @@ const request = fetch('https://restcountries.com/v2/name/portugal'); // this wil
 // then method always returns a promise. nomatter what we return from the callback function. if we return a value, the next then method will receive that value as an argument. if we return a promise, the next then method will wait for that promise to be fulfilled and then it will receive the resulting value as an argument.
 
 // when returining a value, that value will become the fulfilled value of the promise returned by the then method.
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => {
+//       console.log(response);
 
+//       // we can check if the response is ok or not using the ok property of the response object. it will be true if the response is ok and false if not.
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       // any error thrown in the callback function of the then method will terminate that then and will be caught by the catch method of the promise.
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       // country 2
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//       // return 23; // this will become the fulfilled value of the promise returned by the then method
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
+// refactor the above code using getJson function
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
+  // Country 1
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    'Country not found'
+  )
     .then(data => {
       renderCountry(data[0]);
-      const neighbour = data[0].borders?.[0];
-      // country 2
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-      // return 23; // this will become the fulfilled value of the promise returned by the then method
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) throw new Error('No neighbour found!');
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
+
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
       console.error(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
-    }).finally(() => {
-        countriesContainer.style.opacity = 1;
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
 };
+
 // thats a flat chain of promises.
 
 /////////////////////////////////////////////////////////////////////
@@ -213,7 +259,10 @@ const getCountryData = function (country) {
 // we can handle rejected promises using the catch method. it will catch any error that happens in any of the promises in the chain.
 // errors basically propagate down the chain until they are caught, and only if they are not caught, they will be thrown to the outside of the promise chain, where they will become uncaught errors.
 
-
 btn.addEventListener('click', function () {
   getCountryData('portugal');
 });
+
+// getCountryData('ausdsfsdsd');
+
+
